80ed8cf3869bb04ef423b837414e20446fb4e2f0
Raul Sanchez Redondo <raulsredondo2@gmail.com>
2024-10-02 11:22:51 +0200

    Fin Seccion 6

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,20 +2,23 @@ import { NgModule } from '@angular/core';
 
 import { BrowserModule } from '@angular/platform-browser';
 import { CounterModule } from './counter/counter.module';
+import { HeroresModule } from './heroes/heroes.module';
+import { DbzModule } from './dbz/dbz.module';
 
 import { AppComponent } from './app.component';
-import { HeroresModule } from './heroes/heroes.module';
+
+
 
 
 @NgModule({
   declarations: [
     AppComponent,
-
   ],
   imports: [
     BrowserModule,
     CounterModule,
-    HeroresModule
+    HeroresModule,
+    DbzModule
   ],
   providers: [],
   bootstrap: [AppComponent]
